4f4cfa76f7ec8b58c6299c15c4fc1a67f6952613
arxhiewt <arxhie08@gmail.com>
2026-04-03 10:07:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// highlight navbar link based on scroll
-const sections = document.querySelectorAll('.section');
-const navLinks = document.querySelectorAll('.navbar a');
-
-window.addEventListener('scroll', () => {
-    let current = '';
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - 120;
-        if (scrollY >= sectionTop) current = section.getAttribute('id');
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href') === '#' + current) link.classList.add('active');
-    });
-});
+function copy(text) {
+    navigator.clipboard.writeText(text);
+    alert("Copied: " + text);
+}
